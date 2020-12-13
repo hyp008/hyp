7a840f98a7b6581d7a869e7aee84e053be9cd1f9
hyp_glb <hyp_glb@qq.com>
2020-12-13 12:55:57 +0800

    Git demo second commit

--- src/com/hyp/GitDemo.java
+++ src/com/hyp/GitDemo.java
@@ -3,5 +3,8 @@ package com.hyp;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("git---");
+
+        System.out.println("-----change1");
+        System.out.println("change2");
     }
 }
